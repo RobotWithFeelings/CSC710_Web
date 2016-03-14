764342d2f42a6b7bbe08a6a54a4d849d7e94d4ea
Matthew <meneal@gmail.com>
2016-03-14 15:11:52 -0400

    fixed loading weirdness

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express');
 
 var app = express();
 
+
+app.use('/', express.static('public'));
 app.set('port', (process.env.PORT || 3000));
 
-app.get('/', function(request,response){
-	response.sendFile(__dirname + '/index.html')
-})
+// app.get('/', function(request,response){
+// 	response.sendFile('/index.html')
+// });
 
 app.listen(app.get('port'), function() {
 	console.log('Listening on port ' + app.get('port'));
