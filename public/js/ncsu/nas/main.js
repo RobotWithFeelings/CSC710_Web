0be13b4a960e417a5500817a427535e2e1389616
Matthew <meneal@gmail.com>
2016-03-27 18:16:20 -0400

    small change to headers

--- public/js/ncsu/nas/main.js
+++ public/js/ncsu/nas/main.js
@@ -328,7 +328,7 @@ var Main = (function() {
 
 				$.ajax({ 
 					headers:{
-						"Authorization": "Basic " + btoa("695ZFSBY8MXXR5VSZD0656O9P" + ":" + "7y8eMAp1rGzz83H100VBr8OG7JDM6CmpLJHEr6SP6Q8")); 
+						"Authorization": "Basic " + btoa("695ZFSBY8MXXR5VSZD0656O9P" + ":" + "7y8eMAp1rGzz83H100VBr8OG7JDM6CmpLJHEr6SP6Q8")
 					},
 					url: "http://nassdb.herokuapp.com/api/v1/surveys/",
 					type: "POST",
